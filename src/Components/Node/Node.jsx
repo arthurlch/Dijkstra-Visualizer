05f7c5ac53fbaeca4e4733597df9f0b7d5f697be
arthurlch <lachat.arthur74@gmail.com>
2022-01-23 15:28:04 +0900

    fix: forgot node component props and define id

--- src/Components/Node/Node.jsx
+++ src/Components/Node/Node.jsx
@@ -7,8 +7,26 @@ export default class Node extends Component {
     this.state = {};
   }
   render() {
-    const { isFinish, isStart } = this.props;
-    const exClassName = isFinish ? 'node-end' : isStart ? 'node-start' : '';
-    return <div className={`node ${exClassName}`}></div>;
+    const {
+      isFinish,
+      isStart,
+      col,
+      row,
+      isWall,
+      onMouseDown,
+      onMouseENter,
+      onMouseUp,
+    } = this.props;
+
+    const exClassName = isFinish
+      ? 'node-finish'
+      : isStart
+      ? 'node-start'
+      : isWall
+      ? 'node-wall'
+      : '';
+    return (
+      <div className={`node ${exClassName}`} id={`node ${row}-${col}`}></div>
+    );
   }
 }
